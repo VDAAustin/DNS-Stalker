e656ce384e0b91c65cbe8058f0cc2b918fbd7650
mwilds <46567313+MwildsAtVDALabs@users.noreply.github.com>
2019-11-06 05:39:29 -0800

    Add files via upload

--- comparer.sh
+++ comparer.sh
@@ -8,7 +8,7 @@ if cmp -s "$file1" "$file2"; then
 else
 	printf 'The file "%s" is different than "%s" and requires admin to inspect DNS-Stalkers records\n' "$file1" "$file2"
 
-mail -s 'Domain Registry Alert' -t 'mattwilds.bigc@gmail.com' -a From:Admin\<mattwilds.bigc@gmail.com\> <<< ' 
+mail -s 'Domain Registry Alert' -t 'your_email@here.com' -a From:Admin\<your_domain@here.com\> <<< ' 
 This is an automated alert to let you know a possibly malicious was registered. Please log into the linux box and inspect /home/ossec/Downloads/dnstwist-master/outfile.txt.'
 
 
